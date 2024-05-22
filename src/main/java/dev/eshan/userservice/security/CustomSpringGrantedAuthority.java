f75290bbcd28db1a3027e80959252ba56eb7056a
Eshan Akash <eshan_akash>
2024-05-22 19:56:34 +0530

    auth server config

---
+++ src/main/java/dev/eshan/userservice/security/CustomSpringGrantedAuthority.java
@@ -0,0 +1,23 @@
+package dev.eshan.userservice.security;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import dev.eshan.userservice.models.Role;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+@Data
+@JsonDeserialize(as = CustomSpringGrantedAuthority.class)
+@NoArgsConstructor
+public class CustomSpringGrantedAuthority implements GrantedAuthority {
+    private Role role;
+    public CustomSpringGrantedAuthority(Role role) {
+        this.role = role;
+    }
+    @Override
+    @JsonIgnore
+    public String getAuthority() {
+        return role.getRole();
+    }
+}
--- src/main/java/dev/eshan/userservice/security/CustomSpringUserDetails.java
+++ src/main/java/dev/eshan/userservice/security/CustomSpringUserDetails.java
@@ -1,11 +1,21 @@
 package dev.eshan.userservice.security;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import dev.eshan.userservice.models.Role;
 import dev.eshan.userservice.models.User;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
+@Data
+@JsonDeserialize(as = CustomSpringUserDetails.class)
+@NoArgsConstructor
 public class CustomSpringUserDetails implements UserDetails {
     private User user;
     public CustomSpringUserDetails(User user) {
@@ -13,36 +23,51 @@ public class CustomSpringUserDetails implements UserDetails {
     }
 
     @Override
+    @JsonIgnore
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        List<CustomSpringGrantedAuthority> customSpringGrantedAuthorities = new ArrayList<>();
+
+        for (Role role : user.getRoles()) {
+            customSpringGrantedAuthorities.add(
+                    new CustomSpringGrantedAuthority(role)
+            );
+        }
+
+        return customSpringGrantedAuthorities;
     }
 
     @Override
+    @JsonIgnore
     public String getPassword() {
         return user.getPassword();
     }
 
     @Override
+    @JsonIgnore
     public String getUsername() {
         return user.getEmail();
     }
 
     @Override
+    @JsonIgnore
     public boolean isAccountNonExpired() {
         return true;
     }
 
     @Override
+    @JsonIgnore
     public boolean isAccountNonLocked() {
         return true;
     }
 
     @Override
+    @JsonIgnore
     public boolean isCredentialsNonExpired() {
         return true;
     }
 
     @Override
+    @JsonIgnore
     public boolean isEnabled() {
         return true;
     }
